050f0ae18ca3985ba90d46b7c6062c81836d2895
github <Aoi-matsumoto@kccs.co.jp>
2025-06-04 15:37:22 +0900

    初回コミット

---
+++ 05/Ex0501.java
@@ -0,0 +1,13 @@
+class Ex0501{
+	public static void main(String[]args){
+		
+		int totalMoney = 300;
+		int getPoint = 9;
+		
+		System.out.println("金額：" + totalMoney + "円");
+		System.out.println("獲得したポイント：" +	getPoint + "ポイント");
+		
+		
+	}
+	
+}
---
+++ 05/Ex0502.java
@@ -0,0 +1,10 @@
+class Ex0502{
+	public static void main(String[]args){
+		
+		final int START_TIME = 9;
+		int nowTime = 7;
+		
+		System.out.println("開始時刻："+ START_TIME + "時");	
+		System.out.println("現在時刻："+ nowTime +"時");	
+	}
+}
